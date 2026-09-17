10cc2d00b83ad232b8f9ce00ab517e12de2c5f60
Vignesh0074 <vignexh.2003@gmail.com>
2026-09-17 18:13:29 +0530

    upadted project

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const currentUserStr = localStorage.getItem('currentUser');
     const currentUser = currentUserStr ? JSON.parse(currentUserStr) : null;
     
-    const isDash = window.location.pathname.includes('student-') || window.location.pathname.includes('admin-');
+    const isDash = window.location.pathname.includes('student-') || window.location.pathname.includes('admin-') || window.location.pathname.includes('my-subjects') || window.location.pathname.includes('dashboard');
     const isAuthPage = window.location.pathname.includes('login') || window.location.pathname.includes('register');
     
     if (isDash && !isAuthPage) {
